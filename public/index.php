c6a34335c2f38a183d841468ee7507087d77bf30
Michael J. Miller <commnerd@gmail.com>
2025-04-24 06:02:07 -0500

    Electron on base Laravel installation

--- public/index.php
+++ public/index.php
@@ -14,6 +14,13 @@ if (file_exists($maintenance = __DIR__.'/../storage/framework/maintenance.php'))
 require __DIR__.'/../vendor/autoload.php';
 
 // Bootstrap Laravel and handle the request...
+
+if (isset($_SERVER['HTTP_USER_AGENT']) && strpos($_SERVER['HTTP_USER_AGENT'], 'Electron') !== false) {
+    $baseUrl = 'file://' . __DIR__;
+} else {
+    $baseUrl = env('APP_URL');
+}
+
 /** @var Application $app */
 $app = require_once __DIR__.'/../bootstrap/app.php';
 
